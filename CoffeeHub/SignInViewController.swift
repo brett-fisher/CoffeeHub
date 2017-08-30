854aa2fc46a66f611a0ede4ae4280d2b96d3c4eb
Brett Fisher <bdf5106@psu.edu>
2017-08-30 00:29:55 -0400

    Added video background to sign in screen.

---
+++ CoffeeHub/SignInViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  CoffeeHub
+//
+//  Created by Brett Fisher on 8/23/17.
+//  Copyright © 2017 Fish. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class SignInViewController: UIViewController {
+    
+    var player: AVPlayer?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let videoURL: URL = Bundle.main.url(forResource: "coffee_background", withExtension: "mov")!
+        
+        player = AVPlayer(url: videoURL)
+        player?.actionAtItemEnd = .none
+        player?.isMuted = true
+        
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        playerLayer.zPosition = -1
+        playerLayer.frame = view.frame
+        
+        view.layer.addSublayer(playerLayer)
+        
+        player?.play()
+        
+        //Loop video
+        NotificationCenter.default.addObserver(self, selector: Selector(("loopVideo")), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
+        
+        func loopVideo() {
+            player?.seek(to: kCMTimeZero)
+            player?.play()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
